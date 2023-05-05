793a2d4a28e5d14055a7597e6d52a2f30896ec75
Nikolai Efremov <efremovnikolaus@gmail.com>
2023-05-05 14:26:43 +0200

    first commit

---
+++ src/main/java/org/example/Corruption.java
@@ -0,0 +1,42 @@
+package org.example;
+
+import java.util.List;
+
+public class Corruption {
+
+    public Double calculate(int percent, List<Double> accounts) {
+        validateInputs(percent, accounts);
+        while (accounts.size() != 1) {
+            workMethod(percent, accounts);
+        }
+        return accounts.get(0);
+    }
+
+    private void workMethod(int percent, List<Double> accounts) {
+        Double getFirstValue = getMinValue(accounts);
+        accounts.remove(getFirstValue);
+        Double getSecondValue = getMinValue(accounts);
+        accounts.remove(getSecondValue);
+
+        Double result = calculate(percent, getFirstValue, getSecondValue);
+        accounts.add(result);
+    }
+
+    public Double calculate(int percent, Double getFirstValue, Double getSecondValue) {
+        Double percentSum = (double) ((getFirstValue + getSecondValue)/100)*percent;
+        return getFirstValue + getSecondValue - percentSum;
+    }
+
+    public Double getMinValue(List<Double> accounts) {
+        return accounts.stream().mapToDouble(v -> v).min().getAsDouble();
+    }
+
+    private void validateInputs(int percent, List<Double> accounts) {
+        if (percent <= 0 || percent > 100){
+            throw new IllegalArgumentException("Ooopppsss..Bad percent: " + percent);
+        }
+        if (accounts.isEmpty() || accounts.size() < 2) {
+            throw new IllegalArgumentException("Must be several accounts!");
+        }
+    }
+}
---
+++ src/test/java/org/example/CorruptionTest.java
@@ -0,0 +1,81 @@
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CorruptionTest {
+
+    @Test
+    void calculateFirstTest() {
+        int initialPercent = 5;
+        List<Double> initialAccounts = new ArrayList<>();
+        initialAccounts.add(50d);
+        initialAccounts.add(50d);
+
+        Double actual = new Corruption().calculate(initialPercent, initialAccounts);
+        Double expected = 95d;
+
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    void inputValidationTest() {
+        int initialPercent = -5;
+        List<Double> initialAccounts = new ArrayList<>();
+        initialAccounts.add(50d);
+        initialAccounts.add(50d);
+
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> new Corruption().calculate(initialPercent, initialAccounts));
+
+        int initialPercent1 = 5;
+        List<Double> initialAccounts1 = new ArrayList<>();
+        initialAccounts1.add(50d);
+
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> new Corruption().calculate(initialPercent1, initialAccounts1));
+    }
+
+    @Test
+    void bigListCalculationTest() {
+        int initialPercent = 5;
+        List<Double> initialAccounts = new ArrayList<>();
+        initialAccounts.add(1000d);
+        initialAccounts.add(1100d);
+        initialAccounts.add(1200d);
+        initialAccounts.add(1300d);
+
+        Double actual = new Corruption().calculate(initialPercent, initialAccounts);
+        Double expected = 4151.50d;
+
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    void checkMinValueTest() {
+        List<Double> initialAccounts = new ArrayList<>();
+        initialAccounts.add(1000d);
+        initialAccounts.add(1100d);
+        initialAccounts.add(1200d);
+        initialAccounts.add(1300d);
+
+        Double actual = new Corruption().getMinValue(initialAccounts);
+        Double expected = 1000d;
+
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    void calculateSmallMethodTest() {
+        int initialPercent = 5;
+        Double first = 1000d;
+        Double second = 1100d;
+
+        Double actual = new Corruption().calculate(initialPercent, first, second);
+        Double expected = 1995.0d;
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
